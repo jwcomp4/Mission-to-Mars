dfa6b9ac2363fc6785fcc3216874fa738d2ce12b
jwcomp4 <jwcompton4@gmail.com>
2022-08-12 17:08:30 -0500

    Flask templates with scraping script.

--- scraping.py
+++ scraping.py
@@ -3,103 +3,144 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
-
+import datetime as dt
 import pandas as pd
 
-# Setting up Splinter
+def scrape_all():
+    # Setting up Splinter
 
-executable_path = {'executable_path': ChromeDriverManager().install()}
+    executable_path = {'executable_path': ChromeDriverManager().install()}
 
-browser = Browser('chrome', **executable_path, headless=False)
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+    # This dictionary runs all functions
+    # When create HTML template, create paths to dictionary's values, allows to present data in the template.
+    # Add the date the code was last run.
+
+    data = {
+        "news_title":  news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    browser.quit()
+
+    return data
 
 # assigning the url and instructing browswer to visit that url
+# Refactoring for functions
 
-url = 'https://redplanetscience.com'
+def mars_news(browser):
+    url = 'https://redplanetscience.com'
 
-browser.visit(url)
+    browser.visit(url)
 
-# optional delay for loading the page:
+    # optional delay for loading the page:
 
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-# Now setting up the HTML parser:
+    # Now setting up the HTML parser:
 
-html = browser.html
+    html = browser.html
 
-news_soup = soup(html, 'html.parser')
+    news_soup = soup(html, 'html.parser')
 
-side_elem = news_soup.select_one('div.list_text')
+    # Adding try/except block for error handling
+    try:
 
+        side_elem = news_soup.select_one('div.list_text')
 
+        # beginning scraping
+        # Using .find onto the side_elem variable to look for the specific information within the info the variable holds.
+        # Running this code returns the HTML containing the content and everything else nested within the <div />
 
-# beginning scraping
-# Using .find onto the side_elem variable to look for the specific information within the info the variable holds.
-# Running this code returns the HTML containing the content and everything else nested within the <div />
+        side_elem.find('div', class_='content_title')
 
-side_elem.find('div', class_='content_title')
+        # Only want the title.
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        # the .get_text() function chained to .find() returns only the text and not the HTML tags or elements.
 
-# Only want the title.
-# Use the parent element to find the first 'a' tag and save it as 'news_title'
-# the .get_text() function chained to .find() returns only the text and not the HTML tags or elements.
+        news_title = side_elem.find('div', class_='content_title').get_text()
 
-news_title = side_elem.find('div', class_='content_title').get_text()
+        news_p = side_elem.find('div', class_='article_teaser_body').get_text()
+    
+    except AttributeError:
 
-news_title
+        return None, None
 
+    return news_title, news_p
 
-news_p = side_elem.find('div', class_='article_teaser_body').get_text()
-
-news_p
+   
 
 
 # ### JPL Space Images Featured Images
 
 # Visit the image Space Images Mars website
 
-url = 'https://spaceimages-mars.com/'
+def featured_image(browser):
+    url = 'https://spaceimages-mars.com/'
 
-browser.visit(url)
+    browser.visit(url)
 
-# Finding and Clicking the full image button
-# Chaining indexing at the end of the code telling the browser to click the second button.
+    # Finding and Clicking the full image button
+    # Chaining indexing at the end of the code telling the browser to click the second button.
 
-full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem = browser.find_by_tag('button')[1]
 
-full_image_elem.click()
+    full_image_elem.click()
 
-# Parsing the new page to continue scraping the full-size image URL.
+    # Parsing the new page to continue scraping the full-size image URL.
 
-html = browser.html
+    html = browser.html
 
-img_soup = soup(html, 'html.parser')
+    img_soup = soup(html, 'html.parser')
 
-# Finding the relative image URL
+    try:
+    # Finding the relative image URL
 
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
 
-img_url_rel
+    except AttributeError:
+        return None
+    
+    # Use the base URL to create an absolute URL. 
 
-# Use the base URL to create an absolute URL. 
 
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+  
+    return img_url
 
-img_url
-
+def mars_facts():
 # Scraping the table from Mars facts
 
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
+    try:
 
-df.columns=['description', 'Mars', 'Earth']
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
 
-df.set_index('description', inplace=True)
+    except BaseException:
+        # BaseException is somewhat of a catchall. raised when any built-in exceptions are encountered
+        # and it won't handle any user-defined exceptions.
+        # using here because using Pandas' read_html(), can result in errors other than AttributeError
+        return None
 
-df
+    df.columns=['Description', 'Mars', 'Earth']
 
-df.to_html()
+    df.set_index('Description', inplace=True)
 
-browser.quit()
+   
 
+    return df.to_html()
+
+
+if __name__ == "__main__":
+
+    print(scrape_all())
 
 
 
